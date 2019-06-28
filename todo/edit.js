16ab3bfe5971368e7bffca1aec101f4ecdbab95b
OkamotoTomoyuki <okamototomoyuki1988@gmail.com>
2019-06-29 02:27:57 +0900

    エディット　保存まで

--- todo/edit.js
+++ todo/edit.js
@@ -1,15 +1,16 @@
-window.onload = () => {
+
+window.onload = async () => {
 
     let _updates = [];
 
     const _animationFrame = () => {
-        return new Promise(resolve => window.requestAnimationFrame((delta) => resolve(delta)));
+        return new Promise(resolve => window.requestAnimationFrame((timestamp) => resolve(timestamp)));
     }
     const _routine = async () => {
         while (true) {
             var delta = await _animationFrame();
             for (let _update of _updates) {
-                _update(delta);
+                await _update(delta);
             }
         }
     }
@@ -17,36 +18,54 @@ window.onload = () => {
 
     {
         const $ = (query) => document.querySelector(".todo").querySelector(query);
+        const _COL = "todos";
 
-        const _GIST_URL = "https://api.github.com/gists";
-        const _KEY_SAVE_ID = "SAVE/ID";
-        const _KEY_SAVE_PSWD = "SAVE/PSWD";
-        const _KEY_SAVE_NAME = "SAVE/NAME";
+        const url = new URL(location.href);
+        const params = url.searchParams;
+        const pId = params.get("name");
+        const pPrj = params.get("prj");
 
-        const $id = $(".id");
-        const $pswd = $(".pswd");
-        const $name = $(".name");
         const $text = $(".text");
 
-        const _assignIfNotEmpty = ($input, key) => {
-            let saveValue = localStorage.getItem(key);
-            if (saveValue !== null) {
-                $input.value = saveValue;
-            }
-        };
-        _assignIfNotEmpty($id, _KEY_SAVE_ID);
-        _assignIfNotEmpty($pswd, _KEY_SAVE_PSWD);
-        _assignIfNotEmpty($name, _KEY_SAVE_NAME);
-        const _changeToSave = ($input, key) => (_) => {
-            let inputValue = $input.value;
-            let saveValue = localStorage.getItem(key);
-            if (inputValue !== saveValue) {
-                localStorage.setItem(key, inputValue);
-            }
-        };
-        _updates.push(_changeToSave($id, _KEY_SAVE_ID));
-        _updates.push(_changeToSave($pswd, _KEY_SAVE_PSWD));
-        _updates.push(_changeToSave($name, _KEY_SAVE_NAME));
+        if (pId === null || pPrj === null) {
+            $text.value = "パラメータを入力してください。";
+            return;
+        }
 
+        const db = firebase.firestore();
+        const docRef = db.collection(_COL).doc(pId);
+
+        let text = null;
+        try {
+            let doc = await docRef.get();
+            if (doc.exists) {
+                text = doc.data().text;
+                $text.value = text;
+            }
+        } catch (e) {
+            $text.value = "Error getting document:" + e;
+            return;
+        }
+
+        let prev = null;
+        const _update = async (timestamp) => {
+            if (prev == null) {
+                prev = timestamp;
+                return;
+            }
+            let delta = timestamp - prev;
+            if (delta > 1500) {
+                prev = timestamp;
+                if (text !== $text.value) {
+                    text = $text.value;
+                    try {
+                        await docRef.set({"text" : text});
+                    } catch (e) {
+                        $text.value = "Error post document:" + e;
+                    }
+                }
+            }
+        }
+        _updates.push(_update);
     }
 }
