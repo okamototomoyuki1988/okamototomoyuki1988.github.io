852adc4f536b888a0a54114de784af1b224a6532
test1234@test123.com <test1234@test123.com>
2019-07-04 09:31:08 +0900

    更新

--- todo/view.js
+++ todo/view.js
@@ -16,7 +16,6 @@ window.onload = async () => {
     _routine();
 
     {
-        const $ = (query) => document.querySelector(".todo").querySelector(query);
         const _COL = "todos";
 
         const url = new URL(location.href);
@@ -24,79 +23,137 @@ window.onload = async () => {
         const pId = params.get("id");
         const pHpd = params.get("hpd");
 
-        if (pId === null || pHpd === null) {
+        if (pId === null)
             throw new Error("パラメータを入力してください。");
-        }
 
-        const hpd = Number(pHpd);
+        const hpd = pHpd != null ? Number(pHpd) : 6;
 
         const db = firebase.firestore();
         const docRef = db.collection(_COL).doc(pId);
 
-        let src = null;
-        const doc = await docRef.get();
-        if (doc.exists) {
-            src = doc.data().text;
-        }
+        let dataText = null;
+        let doc = await docRef.get();
+        if (doc.exists)
+            dataText = doc.data().text;
 
         const res = await fetch('https://holidays-jp.github.io/api/v1/date.json');
-        const json = await res.json();
+        const holidayDic = await res.json();
 
-        const rows = [];
-        for (const line of src.split(/\r?\n/)) {
-            const row = {};
-            let wk = null;
-
-            wk = line.replace(/^(x|ｘ)/, "")
-            row.isComplete = line !== wk;
-
-            let name = wk.replace(/^(.*)[\s　]([0-9０-９]*[\.．]?[0-9０-９]+)$/, "$1");
-            row.name = name;
-
-            let hourStr = wk.replace(/^.*[\s　]([0-9０-９]*[\.．]?[0-9０-９]+)$/, "$1");
-            hourStr = hourStr.replace(/[０-９]/g, (s) => String.fromCharCode(s.charCodeAt(0) - 65248));
-            hourStr = hourStr.replace("．", ".");
-            row.hour = Number(hourStr);
-            rows.push(row);
+        class Row {
         }
 
-        let today = moment({ hour: 0, minute: 0, seconds: 0, milliseconds: 0 });
-        let rowDay = today.clone();
-        let addDay = 0;
-        for (let row of rows) {
-            row.from = rowDay.clone();
-            addDay = Math.round(row.hour / hpd);
-            rowDay.add(addDay, "days");
-            row.to = rowDay.clone();
-        }
-        let lastDay = rowDay.clone();
+        const render = async () => {
+            const $thead = $("thead");
+            const $tbody = $("tbody");
 
-        const $ths = $(".ths");
-        let colDay = today.clone();
-        while (colDay.isSameOrBefore(lastDay)) {
-            const $th = document.createElement("th");
-            $th.innerText = colDay.format("DD");
-            $ths.appendChild($th);
-            colDay.add(1, "days");
-        }
+            $thead.find("th:not(.blank)").remove();
+            $tbody.empty();
 
-        const $body = $("tbody");
-        for (let row of rows) {
-            const $tr = document.createElement("tr");
-            $body.appendChild($tr);
-            const $th = document.createElement("th");
-            $tr.appendChild($th);
-            $th.innerText = row.name;
+            const rows = [];
+            for (const line of dataText.split(/\r?\n/)) {
+                const row = new Row();
+                let wk = null;
 
-            let tdDay = today.clone();
-            while (tdDay.isSameOrBefore(lastDay)) {
-                const $td = document.createElement("td");
-                $tr.appendChild($td);
-                if (tdDay.isSameOrAfter(row.from) && tdDay.isSameOrBefore(row.to)) {
-                    $td.innerText = "●";
-                }
-                tdDay.add(1, "days");
+                wk = line.replace(/^(x|ｘ)/, "")
+                row.isComplete = line !== wk;
+                let name = wk.replace(/^(.*)[\s　]([0-9０-９]*[\.．]?[0-9０-９]+)$/, "$1");
+                row.name = name;
+
+                let hourStr = wk.replace(/^.*[\s　]([0-9０-９]*[\.．]?[0-9０-９]+)$/, "$1");
+                hourStr = hourStr.replace(/[０-９]/g, (s) => String.fromCharCode(s.charCodeAt(0) - 65248));
+                hourStr = hourStr.replace("．", ".");
+                row.hour = Number(hourStr);
+                rows.push(row);
             }
+
+            let today = moment({ hour: 0, minute: 0, seconds: 0, milliseconds: 0 });
+            let rowDay = today.clone();
+            let addDay = 0;
+            for (let row of rows) {
+                if (row.isComplete == false) {
+                    row.from = rowDay.clone();
+                    addDay = Math.round(row.hour / hpd);
+                    rowDay.add(addDay, "days");
+                    row.to = rowDay.clone();
+                }
+            }
+            let lastDay = rowDay.clone();
+
+            const $ths = $(".ths");
+            let colDay = today.clone();
+            while (colDay.isSameOrBefore(lastDay)) {
+                const $th = $("<th>");
+                $th.text(colDay.format("DD"));
+                $ths.append($th);
+                let color = null;
+                if (colDay.day() == 0 || colDay.day() == 6 || holidayDic[colDay.format("YYYY-MM-DD")])
+                    color = "#A6A";
+                else if (colDay.day() % 2 == 0)
+                    color = "#442";
+                else
+                    color = "#244";
+
+                $th.css("background-color", color);
+                colDay.add(1, "days");
+            }
+
+            for (let i = 0; i < rows.length; i++) {
+                const row = rows[i];
+
+                const $tr = $("<tr>");
+                $tbody.append($tr);
+                if (row.isComplete) {
+                    $tr.css("display", "none");
+                }
+                const $th = $("<th>");
+                $tr.append($th);
+                const $ck = $("<input>").attr("type", "checkbox");
+                $th.append($ck);
+                $ck.click(() => check(i));
+                $th.append(row.name);
+                $th.css("background-color", i % 2 == 0 ? "#eee" : "#ddd");
+
+                let tdDay = today.clone();
+                while (tdDay.isSameOrBefore(lastDay)) {
+                    const $td = $("<td>");
+                    $tr.append($td);
+                    if (tdDay.isSameOrAfter(row.from) && tdDay.isSameOrBefore(row.to))
+                        $td.html("●");
+                    else
+                        $td.html("&nbsp;");
+
+                    let color = null;
+                    if (tdDay.day() == 0 || tdDay.day() == 6 || holidayDic[tdDay.format("YYYY-MM-DD")])
+                        color = i % 2 == 0 ? "#ede" : "#dcd";
+                    else if (tdDay.day() % 2 == 0)
+                        color = i % 2 == 0 ? "#eed" : "#ddc";
+                    else
+                        color = i % 2 == 0 ? "#dee" : "#cdd";
+
+                    $td.css("background-color", color);
+                    tdDay.add(1, "days");
+                }
+            }
+        }
+        render();
+
+        const check = async (index) => {
+            const lines = dataText.split(/\r?\n/);
+            const newlines = [];
+            for (let i = 0; i < lines.length; i++) {
+                if (i == index)
+                    newlines.push("x" + lines[i]);
+                else
+                    newlines.push(lines[i]);
+            }
+            const newData = newlines.join("\n");
+            await docRef.set({ "text": newData, "date": Date.now() });
+
+            doc = await docRef.get();
+            if (doc.exists)
+                dataText = doc.data().text;
+
+            render();
         }
     }
 }
